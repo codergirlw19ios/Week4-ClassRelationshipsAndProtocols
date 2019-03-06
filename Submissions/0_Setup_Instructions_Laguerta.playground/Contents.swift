b3944aadbf26b39fa21b19b5e2298d92a8a84a40
SLaguerta <slaguerta@alumni.nd.edu>
2019-03-05 22:05:36 -0600

    create Food enum

--- Submissions/0_Setup_Instructions_Laguerta.playground/Contents.swift
+++ Submissions/0_Setup_Instructions_Laguerta.playground/Contents.swift
@@ -8,6 +8,8 @@ enum ConsumptionClassification {
 //: - Write a computed variable called `consumptionType` on the `Food` enum that returns a `ConsumptionClassification` type for each food type appropriate to the food. Use `.herbivore` or `.carnivore` instead of `.omnivore`. Use a switch statement that takes `self` as a parameter to switch over the cases of the enum.
 //: - Write a function on the `ConsumptionClassification` enum called `canEat` which takes a parameter of `Food` and returns a `Bool`. The `Food`'s `consumptionType` and `self` must match to return `true`. Exception: If the `Food`'s `consumptionType` is for a `.carnivore` or `.herbivore`, allow the `.omnivore` to return true.
 //: - - Challenge: This can be achieved by returning a single ternary statement.
-
+enum Food {
+    case chicken, chocolate, pizza, burger
+}
 //:  3.) Write one more enum called `Health` and give it the following cases: `dead`, `ill`, `poor`, `well`, `healthy`.
 //: - Write two computed variables on `Health`. One called `decreasedHealth`, one called `increasedHealth`. Return an optional `Health` enum type if it is possible to decrease or increase in health status beyond the current state. (for instance, if the current state is `ill`, in `decreaseHealth` return `.dead`, in `increaseHealth` return `.poor`. You cannot increase health from `.dead`.)
