6c8ad4321d29bf5c3935816c374995862eb2bee1
SLaguerta <slaguerta@alumni.nd.edu>
2019-03-05 21:55:37 -0600

    laguerta - setup problem 1

--- Submissions/0_Setup_Instructions_Laguerta.playground/Contents.swift
+++ Submissions/0_Setup_Instructions_Laguerta.playground/Contents.swift
@@ -1,7 +1,9 @@
 import UIKit
 //: ## SETUP
 //: 1.)  Write an enum called `ConsumptionClassification` with three cases: `omnivore`, `carnivore`, `herbivore`.
-
+enum ConsumptionClassification {
+    case omnivore, carnivore, herbivore
+}
 //: 2.) Write an enum called `Food` with at least two cases, such as `chicken` and `chocolate`
 //: - Write a computed variable called `consumptionType` on the `Food` enum that returns a `ConsumptionClassification` type for each food type appropriate to the food. Use `.herbivore` or `.carnivore` instead of `.omnivore`. Use a switch statement that takes `self` as a parameter to switch over the cases of the enum.
 //: - Write a function on the `ConsumptionClassification` enum called `canEat` which takes a parameter of `Food` and returns a `Bool`. The `Food`'s `consumptionType` and `self` must match to return `true`. Exception: If the `Food`'s `consumptionType` is for a `.carnivore` or `.herbivore`, allow the `.omnivore` to return true.
