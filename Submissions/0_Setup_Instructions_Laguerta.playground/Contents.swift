3e66c95e18d500561c79fb43120fa640499787ee
SLaguerta <slaguerta@alumni.nd.edu>
2019-03-10 16:32:05 -0500

    Create Health enum

--- Submissions/0_Setup_Instructions_Laguerta.playground/Contents.swift
+++ Submissions/0_Setup_Instructions_Laguerta.playground/Contents.swift
@@ -4,8 +4,15 @@ import UIKit
 enum ConsumptionClassification {
     case omnivore, carnivore, herbivore
     
-    func canEat(food: Food) -> Bool {return (((self == .omnivore && food.consumptionType == .carnivore ) || (self == .omnivore && food.consumptionType == .herbivore) || (self == food.consumptionType)) ? true: false )
+//    my attempt
+//    func canEat(food: Food) -> Bool {return (((self == .omnivore && food.consumptionType == .carnivore ) || (self == .omnivore && food.consumptionType == .herbivore) || (self == food.consumptionType)) ? true: false )
+//    }
+    
+    //cleaner ternary
+    func canEat(food: Food) -> Bool {
+        return self == .omnivore ? true : food.consumptionType == self
     }
+    
 }
 //: 2.) Write an enum called `Food` with at least two cases, such as `chicken` and `chocolate`
 //: - Write a computed variable called `consumptionType` on the `Food` enum that returns a `ConsumptionClassification` type for each food type appropriate to the food. Use `.herbivore` or `.carnivore` instead of `.omnivore`. Use a switch statement that takes `self` as a parameter to switch over the cases of the enum.
@@ -29,3 +36,6 @@ enum Food {
 }
 //:  3.) Write one more enum called `Health` and give it the following cases: `dead`, `ill`, `poor`, `well`, `healthy`.
 //: - Write two computed variables on `Health`. One called `decreasedHealth`, one called `increasedHealth`. Return an optional `Health` enum type if it is possible to decrease or increase in health status beyond the current state. (for instance, if the current state is `ill`, in `decreaseHealth` return `.dead`, in `increaseHealth` return `.poor`. You cannot increase health from `.dead`.)
+enum Health {
+    case dead, ill, poor, well, healthy
+}
